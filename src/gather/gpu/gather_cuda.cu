a54d6c8aedef9a4c57e57a8111e7212507ad049b
YiqiWang128 <1670150797@qq.com>
2025-02-04 01:53:58 +0800

    optimized version

--- src/gather/gpu/gather_cuda.cu
+++ src/gather/gpu/gather_cuda.cu
@@ -3,26 +3,52 @@
 #include <stdio.h>
 #include <stdint.h>
 
+constexpr int BLOCKDIM = 128;
+constexpr int THRESHOLD = 1000;
+
 template <typename T>
-__global__ void gather_kernel(const T* __restrict__ data,
-                              const int64_t* __restrict__ indices,
-                              T* __restrict__ output,
+__global__ void gather_kernel(T const *data, int64_t const* indices,
+                              T *output,
                               int pre_size, int axis_size, int post_size,
-                              int indices_size,
-                              int total_output)
+                              int indices_size, int total_output)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx >= total_output)
         return;
+
     int post = idx % post_size;
     int tmp = idx / post_size;
     int indices_idx = tmp % indices_size;
-    int pre = tmp / indices_size;
+    int pre_idx = tmp / indices_size;
+
     int64_t index_val = indices[indices_idx];
     if (index_val < 0 || index_val >= axis_size)
         return;
-    int data_idx = pre * (axis_size * post_size) + index_val * post_size + post;
+    int data_idx = pre_idx * (axis_size * post_size) + index_val * post_size + post;
     output[idx] = data[data_idx];
+}
+
+template <typename T>
+__global__ void optimized_gather_kernel_2d(const T* __restrict__ input,
+                                           const int64_t* __restrict__ indices,
+                                           T* __restrict__ output,
+                                           int stride, int indexSize)
+{
+    int tid = threadIdx.x; 
+    int idx = blockIdx.y;
+    if (tid >= stride)
+        return;
+
+    int64_t row = indices[idx];
+    int over = (stride + BLOCKDIM - 1) / BLOCKDIM;
+    for (int i = 0; i < over; i++){
+        int col = tid + i * BLOCKDIM;
+        if (col >= stride)
+            break;
+        int out_index = idx * stride + col;
+        int in_index = row * stride + col;
+        output[out_index] = input[in_index];
+    }
 }
 
 extern "C" 
@@ -32,27 +58,54 @@ void gather_cuda(void const* data, void const* indices,
                  int indices_size, int elem_size)
 {
     int total_output = pre_size * indices_size * post_size;
-    int threads = 256;
-    int blocks = (total_output + threads - 1) / threads;
 
-    if (elem_size == 2) {
-        gather_kernel<half><<<blocks, threads>>>(
-            reinterpret_cast<const half*>(data),
-            reinterpret_cast<const int64_t*>(indices),
-            reinterpret_cast<half*>(output),
-            pre_size, axis_size, post_size,
-            indices_size, total_output);
-    } else if (elem_size == 4) {
-        gather_kernel<float><<<blocks, threads>>>(
-            reinterpret_cast<const float*>(data),
-            reinterpret_cast<const int64_t*>(indices),
-            reinterpret_cast<float*>(output),
-            pre_size, axis_size, post_size,
-            indices_size, total_output);
-    } else {
-        printf("Unsupported element size: %d\n", elem_size);
-        return;
+    if (pre_size == 1 && (axis_size * post_size) > THRESHOLD) {
+        dim3 blockDim(BLOCKDIM);
+        dim3 gridDim(1, indices_size);
+        if (elem_size == 2) {
+            optimized_gather_kernel_2d<half><<<gridDim, blockDim>>>(
+                reinterpret_cast<const half*>(data),
+                reinterpret_cast<const int64_t*>(indices),
+                reinterpret_cast<half*>(output),
+                post_size, indices_size);
+        }
+        else if (elem_size == 4) {
+            optimized_gather_kernel_2d<float><<<gridDim, blockDim>>>(
+                reinterpret_cast<const float*>(data),
+                reinterpret_cast<const int64_t*>(indices),
+                reinterpret_cast<float*>(output),
+                post_size, indices_size);
+        }
+        else {
+            printf("Unsupported element size: %d\n", elem_size);
+            return;
+        }
     }
+    else {
+        int threads = 256;
+        int blocks = (total_output + threads - 1) / threads;
+        if (elem_size == 2) {
+            gather_kernel<half><<<blocks, threads>>>(
+                reinterpret_cast<const half*>(data),
+                reinterpret_cast<const int64_t*>(indices),
+                reinterpret_cast<half*>(output),
+                pre_size, axis_size, post_size,
+                indices_size, total_output);
+        }
+        else if (elem_size == 4) {
+            gather_kernel<float><<<blocks, threads>>>(
+                reinterpret_cast<const float*>(data),
+                reinterpret_cast<const int64_t*>(indices),
+                reinterpret_cast<float*>(output),
+                pre_size, axis_size, post_size,
+                indices_size, total_output);
+        }
+        else {
+            printf("Unsupported element size: %d\n", elem_size);
+            return;
+        }
+    }
+
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
         printf("CUDA error in gather_cuda: %s\n", cudaGetErrorString(err));
--- test/gather.py
+++ test/gather.py
@@ -8,6 +8,7 @@ import performance
 lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../build/lib/libmy_library.so')
 lib = ctypes.CDLL(lib_path)
 
+
 lib.gather_cuda.argtypes = [
     ctypes.c_void_p,
     ctypes.c_void_p,
@@ -28,40 +29,43 @@ def gather_torch(rank, axis, inputTensor, indexTensor):
 def test(inputShape, indexShape, axis, test_dtype, device):
     print(f"Testing Gather on {device} with input shape:{inputShape} , index shape:{indexShape}, axis:{axis} ,dtype:{test_dtype}")
     inputTensor = torch.rand(inputShape, device=device, dtype=test_dtype)
+
     index_np = np.random.randint(0, inputShape[axis], size=indexShape).astype(np.int64)
     indexTensor = torch.from_numpy(index_np).to(device)
     
     rank = len(inputShape)
     
     torch_gather_time = performance.CudaProfile((gather_torch, (rank, axis, inputTensor, indexTensor)))
-    
+
 
     inputShape = inputTensor.shape
+
     pre_size = int(np.prod(inputShape[:axis])) if axis > 0 else 1
     post_size = int(np.prod(inputShape[axis+1:])) if axis < len(inputShape)-1 else 1
-    indices_size = int(np.prod(indexTensor.shape))
-
+    indices_size = int(indexTensor.numel())
+    
     outShape = list(indexTensor.shape) + list(inputShape[:axis]) + list(inputShape[axis+1:])
     outTensor = torch.zeros(outShape, device=inputTensor.device, dtype=inputTensor.dtype)
 
-    # 获取指针
     input_ptr = ctypes.cast(inputTensor.data_ptr(), ctypes.c_void_p)
     index_ptr = ctypes.cast(indexTensor.data_ptr(), ctypes.c_void_p)
     output_ptr = ctypes.cast(outTensor.data_ptr(), ctypes.c_void_p)
+
     
     def gather_cuda(rank, axis, inputTensor, indexTensor):
         lib.gather_cuda(input_ptr, index_ptr, output_ptr,
-                        pre_size, inputShape[axis], post_size,
-                        indexTensor.numel(), inputTensor.element_size())
+                pre_size, inputShape[axis], post_size,
+                indexTensor.numel(), inputTensor.element_size())
         return outTensor
+
 
     custom_gather_time = performance.CudaProfile((gather_cuda, (rank, axis, inputTensor, indexTensor)))
     performance.logBenchmark(torch_gather_time, custom_gather_time)
-    
-    
+
+
     ref_output = gather_torch(rank, axis, inputTensor, indexTensor)
     custom_output = gather_cuda(rank, axis, inputTensor, indexTensor)
-
+    
     tmpa = ref_output.to('cpu').numpy().flatten()
     tmpb = custom_output.to('cpu').numpy().flatten()
     atol = np.max(np.abs(tmpa - tmpb))
@@ -74,6 +78,7 @@ if __name__ == '__main__':
     parser.add_argument('--device', choices=['cpu', 'cuda', 'mlu'], required=True, help="Device to run the tests on.")
     args = parser.parse_args()
 
+    
     test_cases = [
         # inputShape , indexShape, axis, test_dtype, device
         ((3, 2), (2, 2), 0, torch.float32, "cuda"),
@@ -86,6 +91,7 @@ if __name__ == '__main__':
         ((50257, 768), (16, 1024), 0, torch.float16, "cuda"),
         ((3, 2, 4, 10), (1, 2, 3, 4), 2, torch.float16, "cuda"),
         ((1, 2, 1, 10, 20), (5, 4, 3, 2, 1), 3, torch.float32, "cuda"),
+
         ((5, 15, 30), (5, 6, 7, 8, 9), 2, torch.float16, "cuda"),
         ((8, 45), (10, 12, 15), 1, torch.float32, "cuda"),
     ]
